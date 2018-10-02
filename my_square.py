d6ec3941d5f88fc7ba86ab8e3d61f98b1031bbf1
David Le <dle95035@yahoo.com>
2018-10-02 04:32:27 -0700

    x->y

--- my_square.py
+++ my_square.py
@@ -1,8 +1,8 @@
-def my_square(x):
+def my_square(y):
 	""" return the square of the number
 	    more on documention. 
 	"""
-	return (x ** 2)
-print(my_square(7))
+	return (y ** 2)
+print(my_square(12))
 
 
