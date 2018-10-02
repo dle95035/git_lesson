8a73a5bfebc350f9aeb18551e14cc6bee69378b9
David Le <dle95035@yahoo.com>
2018-10-02 05:35:46 -0700

    added another function

--- my_square.py
+++ my_square.py
@@ -2,8 +2,12 @@ def my_square(y):
 	""" return the square of the number
 	    more on documention. More doc
 	"""
-
 	return (y ** 2)
+
+def my_square2(x):
+	return (x*x)
+
 print(my_square(12))
+print(my_square2(12))
 
 
