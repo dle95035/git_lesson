570039824fa936aaf09773c0a09cdff5261ffad2
dle95035 <34071015+dle95035@users.noreply.github.com>
2018-10-02 16:32:59 -0700

    make a bug

--- my_square.py
+++ my_square.py
@@ -1,8 +1,8 @@
 def my_square(x):
 	""" return the square of the number
-	    more on documention. 
+	    more on documention. More doc
 	"""
-	return (x ** 2)
+	return (x * 2)
 print(my_square(7))
 
 
