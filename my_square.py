6f4d931054e7d18767377d278018c01a02f1ec56
David Le <dle95035@yahoo.com>
2018-10-02 03:59:45 -0700

    more change to the variables

--- my_square.py
+++ my_square.py
@@ -1,7 +1,7 @@
-def my_square(x):
+def my_square(y):
 	""" return the square of the number
 	    more on documention. change more
 	"""
-	return (x ** 2)
-print(my_square(5))
+	return (y ** 2)
+print(my_square(6))
 
